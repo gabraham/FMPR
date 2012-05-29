ec4d340d56baadf6f560dd1f815c71cf91b1587a
Gad Abraham <gad.abraham@gmail.com>
2012-05-29 13:34:23 +1000

    Deleted old files

--- gflasso.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-N=$(cat X.txt | wc -l)
-p=$(head -1 X.txt | wc -w)
-K=$(head -1 Y.txt | wc -w)
-~/Software/gflasso_bin/gflasso Y.txt X.txt \
-   $N $p $K config.txt gc Ynetwork.txt \
-   gflasso
